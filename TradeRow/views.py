7df22f71de7b4cc1f82d0589f35485e9fd4a8543
pabitra <pabitrapandit2001@gmail.com>
2023-09-06 16:06:51 +0530

    first commit

---
+++ TradeRow/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from category.models import Category
+from store.models import Product
+def home(request):
+    category = Category.objects.all()
+    products  = Product.objects.all().filter(is_avaliable=True)
+    context = {
+        'category': category,
+        'products': products,
+    }
+
+    return render(request, 'home.html',context)
